691027c06efc800ff4af4f135ea604bea0cd02ab
eciggaar <edward@local>
2016-09-07 10:53:11 +0200

    Porting activities

--- app.js
+++ app.js
@@ -15,8 +15,36 @@ var cfenv = require('cfenv');
 // create a new express server
 var app = express();
 
+var vcapServices = require('vcap_services');
+var watson = require('watson-developer-cloud');
+var extend = require('util')._extend;
+
+// For local development, replace username and password
+var config = extend({
+  version: 'v1',
+  url: 'https://stream.watsonplatform.net/speech-to-text/api',
+  username: process.env.STT_USERNAME || '<username>',
+  password: process.env.STT_PASSWORD || '<password>'
+}, vcapServices.getCredentials('speech_to_text'));
+
+var authService = watson.authorization(config);
+
 // serve the files out of ./public as our main files
 app.use(express.static(__dirname + '/public'));
+
+app.get('/token', function(req, res) {
+   authService.getToken({url: config.url}, function(err, token) {
+      if (err) {
+         console.log('error:', err);
+         res.status(err.code);
+      }
+      res.send(token);
+   });
+});
+
+app.post('/sentiment', function(req,res){
+
+});
 
 // get the app environment from Cloud Foundry
 var appEnv = cfenv.getAppEnv();
